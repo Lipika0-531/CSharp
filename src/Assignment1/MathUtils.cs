806f9cabc0386a5f614e3b69739eca178eb34d3d
Lipika Maadeswaran <lipika.maadeswaran@solitontech.com>
2023-08-14 17:22:23 +0530

    feat: assignment1 Adding mathutils class

---
+++ src/Assignment1/MathUtils.cs
@@ -0,0 +1,53 @@
+﻿namespace Assignment1
+{
+    /// <summary>
+    /// MathUtils Class
+    /// </summary>
+    public class MathUtils
+    {
+        /// <summary>
+        /// Add function will perform addition operation
+        /// </summary>
+        /// <param name="x">x denotes variable x</param>
+        /// <param name="y">y denotes variable y</param>
+        /// <returns>it returns x + y</returns>
+        public long Add(int x, int y)
+        {
+            return (long)x + y;
+        }
+        /// <summary>
+        /// Subtraction method would subtract two values 
+        /// </summary>
+        /// <param name="x">x denotes variable x</param>
+        /// <param name="y">y denotes variable y</param>
+        /// <returns>subtract</returns>
+        public long Subtract(int x, int y)
+        {
+            return (long)x - y;
+        }
+        /// <summary>
+        /// Multiply method would multiply two values
+        /// </summary>
+        /// <param name="x">x denotes variable x</param>
+        /// <param name="y">y denotes variable y</param>
+        /// <returns>it returns multiplication of two values</returns>
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        /// <summary>
+        /// Divide method will divide the two values
+        /// </summary>
+        /// <param name="x">x denotes variable x</param>
+        /// <param name="y">y denotes variable y</param>
+        /// <returns>it returns division of two values</returns>
+        public double Divide(int x, int y)
+        {
+            if( y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return x / y;
+        }
+    }
+}
---
+++ src/Assignment1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Xml.Linq;
+
+namespace Assignment1
+{
+    /// <summary>
+    /// Program internal class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main class
+        /// </summary>
+        /// <param name="args"> arguments </param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number (X) : ");
+            string? input1 = Console.ReadLine();
+            if (string.IsNullOrEmpty(input1))
+            {
+                Console.WriteLine("value can't be empty! Input your value once more");
+                input1 = Console.ReadLine();
+            }
+            Console.WriteLine("Enter the number (Y) : ");
+            string? input2 = Console.ReadLine();
+            if (string.IsNullOrEmpty(input2))
+            {
+                Console.WriteLine("value can't be empty! Input your value once more");
+                input1 = Console.ReadLine();
+            }
+            int.TryParse(input1, out int x);
+            int.TryParse(input2, out int y);
+            MathUtils myobj = new MathUtils();
+            Console.WriteLine($"Addition: {myobj.Add(x, y)}");
+            Console.WriteLine($"Subtraction: {myobj.Subtract(x, y)}");
+            Console.WriteLine($"Multiply: {myobj.Multiply(x, y)}");
+            try
+            {
+                double division = myobj.Divide(x, y);
+                Console.WriteLine($"Divide: {division}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"Cannot divide by zero");
+            }     
+        }
+    }
+}

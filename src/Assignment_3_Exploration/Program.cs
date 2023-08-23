3e99bb1330205f03276c9cee319babc875278b40
Lipika Maadeswaran <lipika.maadeswaran@solitontech.com>
2023-08-23 12:17:30 +0530

    feat : assignment3-Exploration completed

---
+++ src/Assignment_3_Exploration/Program.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+namespace Assignment_3_Exploration
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Program class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main method where the program starts
+        /// </summary>
+        /// <param name="args">Argument</param>
+        public static void Main(string[] args)
+        {
+            var json = @"{""Name"":""Lipika"",""ID"":""24018"",""Number"": ""9597629825""}";
+            Product? p = JsonConvert.DeserializeObject<Product>(json) !;
+            Console.WriteLine(p.Name);
+        }
+    }
+}

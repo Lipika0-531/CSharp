a9e567e750269bcb962c97983251b8b60357f0b4
Lipika Maadeswaran <lipika.maadeswaran@solitontech.com>
2023-08-24 15:45:51 +0530

    feat : assignment7 memory optimization completed

---
+++ src/assignment7/Program.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Assignment7
+{
+    /// <summary>
+    /// Program class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main class
+        /// </summary>
+        /// <param name="args">arguments</param>
+        public static void Main(string[] args)
+        {
+            MemoryEater me = new MemoryEater();
+            me.Allocate();
+            Console.WriteLine("Completed!");
+            Console.ReadKey();
+        }
+    }
+}

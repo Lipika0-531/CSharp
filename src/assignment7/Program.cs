deb7a0f95220ecc31c6a7d27722aaa04bd5d5d2a
Lipika Maadeswaran <lipika.maadeswaran@solitontech.com>
2023-08-31 09:49:42 +0530

    fix : assignment7 modified comments

--- src/assignment7/Program.cs
+++ src/assignment7/Program.cs
@@ -5,14 +5,14 @@
 namespace Assignment7
 {
     /// <summary>
-    /// Program class
+    /// Program class is the base class
     /// </summary>
     internal class Program
     {
         /// <summary>
         /// Main class is initiated
         /// </summary>
-        /// <param name="args">arguments</param>
+        /// <param name="args">Console Line arguments</param>
         public static void Main(string[] args)
         {
             MemoryEater me = new MemoryEater();

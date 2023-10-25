95d7062395766f9766c887a756ebcf97114686bd
Lipika Maadeswaran <lipika.maadeswaran@solitontech.com>
2023-10-25 19:24:04 +0530

    fix : bugs fixed

---
+++ src/Assign7/Assignment7/Program.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Assignment7
+{
+    /// <summary>
+    /// Program class for Assignment7
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Driver method.
+        /// </summary>
+        /// <param name="args">arguments passed</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
